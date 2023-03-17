b15b99bf516701860b2941e7b9e2ac3bf3907b15
Joel Valerio <jev66@cornell.edu>
2023-03-17 00:43:55 -0400

    Completed mli and implemented some initializers

--- src/battleship.ml
+++ src/battleship.ml
@@ -1,15 +1,47 @@
 (* Implement functions from battleship.mli *)
-open Graphics
+open GMain
+open Consts
 
-type cell = 
-  (** type cell represents a cell within the board. *)
-{
-  point : int * int;
-  width : int;
-  height : int;
-  color : Graphics.color;
+exception InvalidPosition
+
+type ship = {
+  length : int;
+  mutable hits : int;
 }
 
-type t = { cells : cell list list }
+type cell =
+  | Empty
+  | Ship of ship ref
+  | Hit
+  | Miss
 
-let rec board_init (i : int) = raise (Failure "Unimplemented")
+type board = cell list list
+
+type player = {
+  board : board;
+  ships : ship list;
+}
+
+type game = {
+  players : player list;
+  mutable current_player : int;
+}
+
+let init_board () =
+  List.init cBOARD_SIZE (fun _ -> List.init cBOARD_SIZE (fun _ -> Empty))
+
+let init_ship len = { length = len; hits = 0 }
+
+let create_ship x =
+  match x with
+  | 0 -> init_ship cCARRIER
+  | 1 | 2 -> init_ship cDESTROYER
+  | 3 | 4 | 5 -> init_ship cSUBMARINE
+  | 6 | 7 | 8 | 9 -> init_ship cPATROL
+  | _ -> raise (invalid_arg "Too many ships")
+
+let init_player () = { board = init_board (); ships = List.init 10 create_ship }
+let place_ship player ship row col dir = { player with board = init_board () }
+let fire board row col = raise (Failure "init_board Unimplemented")
+let get_ships player = player.ships
+let is_game_over player = raise (Failure "init_board Unimplemented")
--- src/battleship.mli
+++ src/battleship.mli
@@ -1,6 +1,43 @@
-type t
-(** The abstract type representing the game board. *)
+type cell
+(** Type representing a cell on the board *)
 
-val board_init : int -> t
-(** [board_init i] is the board for the game of size [i] by [i]. Requires:
-    [i <= 12]*)
+type board
+(** Type representing the game board, it is a 10x10 grid *)
+
+type ship
+(** Type representing a ship on the board *)
+
+type player
+(** Type representing a player. This refers to their board and their ships. *)
+
+exception InvalidPosition
+
+(* type game = { players : player list; mutable current_player : int; } *)
+(** Type representing the game itself. *)
+
+val init_board : unit -> board
+(** [init_board ()] initializes a new, empty game board. *)
+
+val init_ship : int -> ship
+(** [init_ship ()] initializes a new ship *)
+
+val init_player : unit -> player
+(** [init_plyaer ()] initializes a player with an empty board *)
+
+(* val init_game : unit -> game *)
+
+val place_ship : player -> ship -> int -> int -> int -> player
+(** [place_ship board ship row col dir] is the board after a ship has been
+    placed in ([row],[col]) facing direction [dir]. [dir] is 0 if the ship is
+    horizontal, 1 if vertical. Requires: [dir] is 0 of 1.*)
+
+val fire : board -> int -> int -> board
+(** [fire board row col] is the updated [board] after a shot is fired at
+    position ([row],[col]) on the board *)
+
+val get_ships : player -> ship list
+(** [get_ships player] is the list of all ships belonging to the [player]*)
+
+val is_game_over : player -> bool
+(** [is_game_over player] is whether all of [player]'s ships have been
+    destroyed. *)
