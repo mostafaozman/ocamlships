1680224f6016533e9cf02f506546f8b2bd16d04a
MostafaOsman <mo379@cornell.edu>
2023-03-16 20:55:16 -0400

    Name the  graph and get it to stay on the screen

--- src/gui.ml
+++ src/gui.ml
@@ -1,5 +1,10 @@
 (* Implement functions from gui.mli *)
 open Graphics
 
-let start_game () = open_graph " 800x800"
+let start_game () =
+  let graph = open_graph " 800x800" in
+  while true do
+    ()
+  done
+
 let quit () = close_graph
