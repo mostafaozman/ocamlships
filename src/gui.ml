5f88ff7fdad8c63faa7267349ea9f0fe2c9f5041
Mostafa Osman <mo379@cornell.edu>
2023-03-20 15:03:58 -0400

    Beautified the start screen  - Used different font and font size

--- src/gui.ml
+++ src/gui.ml
@@ -5,8 +5,8 @@ let go_green = 0x1B512D
 let quit_red = 0x94241a
 let logo_wht = 0xF7F7F2
 
-let write size mv_x mv_y color string =
-  set_text_size size;
+let write mv_x mv_y color string =
+  set_font "-*-fixed-medium-r-semicondensed--50-*-*-*-*-*-iso8859-1";
   moveto mv_x mv_y;
   set_color white;
   draw_string string
@@ -17,13 +17,13 @@ let draw_btn color x y width height =
 
 let home () =
   draw_btn go_green 200 300 400 125;
-  write 20 375 350 white "Start Game";
+  write 300 340 white "Start Game";
 
   draw_btn quit_red 200 100 400 125;
-  write 20 390 150 white "Quit";
+  write 355 140 white "Quit";
 
   draw_btn black 0 620 800 280;
-  write 50 370 700 white "Battle Ships"
+  write 280 680 white "Battle Ships"
 
 let quit () = exit 0
 let go_start () = clear_graph ()
