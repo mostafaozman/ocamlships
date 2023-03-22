1df063b67f0c53e4af6cec23b078391437c4bd29
Tahmidul <ta278@cornell.edu>
2023-03-22 11:53:44 -0400

    Renamed and organized test suites

--- test/main.ml
+++ test/main.ml
@@ -5,8 +5,8 @@ open Ships.Battleship
    Helper functions.
  ********************************************************************)
 
-let example_test_function (name : string) (input : char) (expected_output : int)
-    : test =
+let init_board_test (name : string) (input : char) (expected_output : int) :
+    test =
   name >:: fun _ ->
   assert_equal expected_output (int_of_char input) ~printer:string_of_int
 
@@ -15,16 +15,19 @@ let example_test_function (name : string) (input : char) (expected_output : int)
  ********************************************************************)
 
 (* example test suites *)
-let suite1_tests =
+let board = init_board
+
+let init_tests =
   [
     ( "Initialize ship test" >:: fun _ ->
       assert_equal 4 (init_ship 4 |> get_ship_length) );
   ]
 
-let suite2_tests = []
+let getter_tests = []
+let func_test = []
 
 let suite =
   "test suite for final project"
-  >::: List.flatten [ suite1_tests; suite2_tests ]
+  >::: List.flatten [ init_tests; getter_tests; func_test ]
 
 let _ = run_test_tt_main suite
