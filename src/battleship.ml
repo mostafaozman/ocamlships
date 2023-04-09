3fa2a80e214cd6fbb197c0aa0ada61d3640784ac
Joel Valerio <jev66@cornell.edu>
2023-04-08 22:39:24 -0400

    Removed constants mistakenly placed in battleship.ml

--- src/battleship.ml
+++ src/battleship.ml
@@ -1,25 +1,5 @@
 (* Implement functions from battleship.mli *)
-let board_size = 14
-let carrier = 5
-let destroyer = 4
-let submarine = 3
-let patrol = 2
-let carrier_num = 1
-let destroyer_num = 2
-let submarine_num = 3
-let patrol_num = 4
-let box_size = 45
-let num_box = 13
-let box_off = 4
-let background_llx = 70
-let background_lly = 120
-let background_length = 634
-let background_tly = background_lly + background_length
-let go_green = 0x1B512D
-let quit_red = 0x94241a
-let logo_wht = 0xF7F7F2
-let ocean_blue = 0x7BB5FF
-let rotate = "⟳"
+open Consts
 
 exception InvalidPosition of string
 
