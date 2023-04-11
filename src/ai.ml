49cb29fe73dd1c2c845ec20079cfc5354af361a5
Joel Valerio <jev66@cornell.edu>
2023-04-11 19:43:30 -0400

    Made placing and fire for AI

---
+++ src/ai.ml
@@ -0,0 +1,24 @@
+open Battleship
+open Consts
+module R = Random
+
+let create_placements p =
+  let rec helper (sz : int) (p : player) =
+    try
+      place_ship p (init_ship sz) (R.int board_size) (R.int board_size)
+        (R.int 2)
+    with exn -> helper sz p
+  in
+  let rec loop (i : int) (acc : player) =
+    match i with
+    | 0 -> acc
+    | n when n < patrol_num -> loop (i - 1) (helper patrol acc)
+    | n when n < patrol_num + submarine_num ->
+        loop (i - 1) (helper submarine acc)
+    | n when n < patrol_num + submarine_num + destroyer_num ->
+        loop (i - 1) (helper destroyer acc)
+    | _ -> loop (i - 1) (helper carrier acc)
+  in
+  loop (carrier_num + destroyer_num + submarine_num + patrol_num) p
+
+let ai_fire board = fire board (R.int board_size) (R.int board_size)
